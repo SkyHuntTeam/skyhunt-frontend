aa1c4061e6e663c94145ae3d39286dc6e13637b4
Lidka <75801399+lidiamoryc@users.noreply.github.com>
2023-11-21 04:29:13 +0400

    poprawa wygladu kodu

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,12 @@
+//landing page - strona początkowa
+
 import logo3 from './assets/logo3.png'
 import { NavLink } from 'react-router-dom';
+
+
 export const Home = () => {
     return(
+
         <div>
         <div className="c_home">
             <h2>SkyHunt.</h2>
@@ -9,13 +14,9 @@ export const Home = () => {
             <button className="b_h">
                 <NavLink to='/main' style={{ color: 'white' }}>Rozpocznij</NavLink>
             </button>
-            {/* <nav>
-            
-                    <NavLink to='Mainp'></NavLink>
-                </nav> */}
-            
             <img className="img_h" src={logo3}  alt="logo"/>
         </div>
+
         <div className='napis_home2'>
         <p >SkuHunt to aplikacja internetowa umożliwiającaj rezerwację lotów. Wyszukiwarka poda dostępne opcje lotów na podstawie destynacji, daty, możliwych przesiadek
                  oraz ilości osób. Użytkownik będzie mógł utworzyć konto, na którym będą przechowywane dokonane rezerwacje oraz bieżące saldo.</p>
@@ -23,4 +24,5 @@ export const Home = () => {
         </div>
     )
 }
+
 export default Home
